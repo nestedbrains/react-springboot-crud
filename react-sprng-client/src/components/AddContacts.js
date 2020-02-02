b361a09014d0a30367281b2f9ce348ed39d467d6
ovi <ovi@bitmascot.com>
2020-02-02 10:52:45 +0600

    save data

--- react-sprng-client/src/components/AddContacts.js
+++ react-sprng-client/src/components/AddContacts.js
@@ -2,15 +2,17 @@ import React, { Component } from 'react';
 
 export default class AddContacts extends Component {
 
+
     submitContact(event) {
         event.preventDefault();
 
         let contact = {
             name: this.refs.name.value,
             address: this.refs.address.value,
-            phone: this.refs.phone.value,
+            phone: this.refs.phoneNo.value,
             email: this.refs.email.value
         }
+        console.log(this.refs)
 
         fetch("http://localhost:8080/v1/api/students",
             {
@@ -28,39 +30,39 @@ export default class AddContacts extends Component {
     render() {
         return (
             <div className="row">
-                <form className="col s12" onSubmit={this.submitContact.bind(this)}>
+            <form className="col s12" onSubmit={this.submitContact.bind(this)}>
 
-                    <div className="row">
-                        <div className="input-field col s12">
-                            <input id="name" type="text" className="validate" />
-                            <label htmlFor="name">Name</label>
-                        </div>
-                    </div>
-
-                    <div className="row">
-                        <div className="input-field col s12">
-                            <input id="email" type="email" className="validate" />
-                            <label htmlFor="email">Email</label>
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="input-field col s12">
-                            <input id="phoneNo" type="text" className="validate" />
-                            <label htmlFor="phoneNo">Phone No</label>
-                        </div>
-                    </div>
-
-                    <div className="row">
-                        <div className="input-field col s12">
-                            <input id="address" type="text" className="validate" />
-                            <label htmlFor="address">Address</label>
-                        </div>
-                    </div>
-                    <div className="row">
-                        <button className="waves-effect waves-light btn " type="submit" name="action">Submit</button>
-                    </div>
-                </form>
+    <div className="row">
+            <div className="input-field col s12">
+            <input id="name" type="text" ref="name" className="validate" />
+            <label htmlFor="name">Name</label>
             </div>
-        )
+            </div>
+
+            <div className="row">
+            <div className="input-field col s12">
+            <input id="email" type="email" ref="email" className="validate" />
+            <label htmlFor="email">Email</label>
+            </div>
+            </div>
+            <div className="row">
+            <div className="input-field col s12">
+            <input id="phoneNo" type="text" ref="phoneNo" className="validate" />
+            <label htmlFor="phoneNo">Phone No</label>
+        </div>
+        </div>
+
+        <div className="row">
+            <div className="input-field col s12">
+            <input id="address" type="text" ref="address" className="validate" />
+            <label htmlFor="address">Address</label>
+            </div>
+            </div>
+            <div className="row">
+            <button className="waves-effect waves-light btn " type="submit" name="action">Submit</button>
+            </div>
+            </form>
+            </div>
+    )
     }
 }
--- react-sprng-client/src/components/SingleContact.js
+++ react-sprng-client/src/components/SingleContact.js
@@ -1,20 +1,22 @@
 import React from 'react';
 
 const SingleContact = ({ item }) => {
+
     return (
-        <div className="row">
-            <div className="col s12 m6">
-                <div className="card blue-grey darken-1">
-                    <div className="card-content white-text">
-                        <span className="card-title">{item.name}</span>
-                    </div>
-                    <div className="card-action">
-                        <p>{item.email}</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    )
+        <div className="col s12 m6 l3">
+
+        <a class="btn tooltipped" href="/" data-position="top" data-tooltip="Edit"><i class="material-icons">edit</i></a>
+    <div className="card blue-grey">
+        <div className="card-content white-text">
+        <span className="card-title">{item.name}</span>
+    </div>
+    <div className="card-action">
+        <p>{item.email}</p>
+    </div>
+    </div>
+    </div>
+
+)
 };
 
 export default SingleContact;
